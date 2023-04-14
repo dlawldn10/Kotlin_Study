b697837b46b946d5654322a458f6a72fcc19ea17
Jiwoo Lim <dlawldn10@gmail.com>
2023-04-14 22:27:38 +0900

    코틀린 연습

--- app/src/main/java/com/ivy/kotlin_study/BaekjoonPrac.kt
+++ app/src/main/java/com/ivy/kotlin_study/BaekjoonPrac.kt
@@ -1,7 +1,8 @@
 package com.ivy.kotlin_study
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.*
-import kotlin.math.max
 
 
 // 13458 - 시험감독
@@ -456,85 +457,316 @@ import kotlin.math.max
 // 3085 - 사탕 게임
 // 11:43
 // --
-// 시간초과
-fun main(){
-    val sc = Scanner(System.`in`)
-    val N = sc.nextInt()
-    val adj = mutableListOf<MutableList<Char>>()
-    repeat(N){
-        val row = sc.next().toMutableList()
-        adj.add(row)
-    }
-    var answer = 0
+// 시간초과 -> 재귀를 단순 반복으로 변경하여 해결
+//fun main(){
+//    val sc = Scanner(System.`in`)
+//    val N = sc.nextInt()
+//    val adj = mutableListOf<MutableList<Char>>()
+//    repeat(N){
+//        val row = sc.next().toMutableList()
+//        adj.add(row)
+//    }
+//    var answer = 0
+//
+//    fun search(){
+//        // 가로 일렬 탐색
+//        var count = 0
+//        for(i in 0 until N){
+//            count = 1
+//            for (j in 0 until N-1){
+//                if (adj[i][j] == adj[i][j+1]){
+//                    count++
+//                    answer = max(answer, count)
+//                }
+//                else {
+//                    count = 1
+//                }
+//            }
+//        }
+//
+//        // 세로 일렬 탐색 for문 i, j 바꾸기!!
+//        for(j in 0 until N){
+//            count = 1
+//            for (i in 0 until N-1){
+//                if (adj[i][j] == adj[i+1][j]) {
+//                    count++
+//                    answer = max(answer, count)
+//                }
+//                else {
+//                    count = 1
+//                }
+//            }
+//        }
+//
+//    }
+//
+//
+//    for (i in 0 until N){
+//        for (j in 0 until N){
+//            // 양옆 스왑
+//            if (j+1 < N){
+//                val a = adj[i][j]
+//                val b = adj[i][j+1]
+//                // 서로 스왑하고
+//                adj[i][j] = b
+//                adj[i][j+1] = a
+//                // 가장 긴 연결 요소 구하기
+//                search()
+//                // 스왑한거 되돌려놓기
+//                adj[i][j] = a
+//                adj[i][j+1] = b
+//            }
+//
+//            // 위아래 스왑
+//            if (i+1 < N){
+//                val a = adj[i][j]
+//                val b = adj[i+1][j]
+//                // 서로 스왑하고
+//                adj[i][j] = b
+//                adj[i+1][j] = a
+//                // 가장 긴 연결 요소 구하기
+//                search()
+//                // 스왑한거 되돌려놓기
+//                adj[i][j] = a
+//                adj[i+1][j] = b
+//            }
+//        }
+//
+//    }
+//
+//    println(answer)
+//
+//}
 
-    fun search(){
-        // 가로 일렬 탐색
-        var count = 0
-        for(i in 0 until N){
-            count = 1
-            for (j in 0 until N-1){
-                if (adj[i][j] == adj[i][j+1]){
-                    count++
-                    answer = max(answer, count)
-                }
-                else {
-                    count = 1
-                }
+
+// 1463 - 1로 만들기
+// 2:06
+// 3:14 - 답 참고
+// DP 풀이
+//fun main(){
+//    val sc = Scanner(System.`in`)
+//    val N = sc.nextInt()
+//    val adj = Array(N+1){0}
+//    adj[1] = 0
+//
+//    var num = 2
+//    var X = 2
+//    while (X <= N){
+//        if (num % 6 == 0){
+//            num /= if(adj[num/3] <= adj[num/2]) 3 else 2
+//        }
+//        else if (num % 3 == 0){
+//            if(adj[num/3] <= adj[num-1]) num /= 3 else num -= 1
+//        }
+//        else if (num % 2 == 0){
+//            if(adj[num/2] <= adj[num-1]) num /= 2 else num -= 1
+//        }
+//        else num -= 1
+//
+//        if (num == 1) adj[X] = 1
+//        else adj[X] = adj[num] + 1
+//
+//        // 다음으로 넘어가기
+//        X++
+//        num = X
+//
+//    }
+//
+////    println(adj.contentToString())
+//    println(adj[N])
+//}
+
+// BFS 풀이
+//fun main(){
+//    val sc = Scanner(System.`in`)
+//    val N = sc.nextInt()
+//    val queue = LinkedList<Pair<Int, Int>>()
+//    // (현재 값, 몇번 건너왔는지)
+//    queue.add(Pair(N, 0))
+//    val check = IntArray(N+1){ 0 }
+//    check[N] = 1
+//
+//    while (queue.isNotEmpty()){
+//        val (x,  d) = queue.poll()
+//
+//        if (x == 1){
+//            println(d)
+//            return
+//        }
+//
+//        if (x % 3 == 0 && check[x/3] == 0){
+//            queue.add(Pair(x/3, d+1))
+//            check[x/3] = 1
+//        }
+//
+//        if (x % 2 == 0 && check[x/2] == 0){
+//            queue.add(Pair(x/2, d+1))
+//            check[x/2] = 1
+//        }
+//
+//        if (check[x-1] == 0){
+//            queue.add(Pair(x-1, d+1))
+//            check[x-1] = 1
+//        }
+//    }
+//
+//}
+
+
+// 11726 - 2xn 타일링
+// 3:41
+// 4:05
+// DP 재귀 풀이
+//fun main(){
+//    val sc = Scanner(System.`in`)
+//    val n = sc.nextInt()
+//
+//    if (n == 1) {
+//        println(1)
+//        return
+//    }
+//    else if (n == 2) {
+//        println(2)
+//        return
+//    }
+//
+//    val cache = IntArray(n+1){0}
+//    cache[1] = 1
+//    cache[2] = 2
+//
+//    fun dp(x: Int): Int{
+//        if (cache[x] != 0) return cache[x]
+//
+//        cache[x] = (dp(x-2) + dp(x-1)) % 10007
+//        return cache[x]
+//    }
+//
+//    println(dp(n))
+////    println(cache.contentToString())
+//}
+
+// DP 반복문 풀이
+//fun main(){
+//    val sc = Scanner(System.`in`)
+//    val n = sc.nextInt()
+//
+//    val cache = IntArray(n+1){0}
+//    cache[1] = 1
+//    cache[2] = 2
+//
+//    for (x in 3..n){
+//        cache[x] = (cache[x-2] + cache[x-1]) % 10007
+//    }
+//
+//    println(cache[n])
+////    println(cache.contentToString())
+//}
+
+
+// 3687 - 성냥개비
+// 8:00
+// --
+// 정답 코드
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n: Int = br.readLine().toInt()
+    for (i in 0 until n) {
+        val matchesCount: Int = br.readLine().toInt()
+        val minDp = LongArray(101)
+
+
+        //성냥 한개를 표현하는데 사용하는 개수는 2~7개 사이
+        //2개 : 1, 3개 : 7, 4개 : 4, 5개 : 2,3,5, 6개 : 6,9,0, 7개 : 8
+
+        /*
+		* mindp[i]는 i개의 성냥개비를 사용하여 만들 수 있는
+		* 가장 작은 수를 저장하는 배열, 인덱스 2~8까지는 초기화 필요!
+		*
+		* 이때 mindp[6]으로 만들 수 있는 숫자는 0,6 두 가지가 있지만
+		* 0은 첫번째 자리에 올 수 없으므로 mindp[6]은 6으로 초기화 한다.
+		* 다른 숫자 뒤에 올 때는 6보다 작은 0을 사용한다.
+		*/
+        Arrays.fill(minDp, Long.MAX_VALUE)
+        minDp[2] = 1
+        minDp[3] = 7
+        minDp[4] = 4
+        minDp[5] = 2
+        minDp[6] = 6
+        minDp[7] = 8
+        minDp[8] = 10
+        val add = arrayOf("1", "7", "4", "2", "0", "8")
+        /*
+         * 성냥개비 9개로 만들 수 있는 가장 작은수를 구하기 위한 경우의 수에는
+         * 2+7, 3+6, 4+5, 5+4, 6+3, 7+2가 있다.
+         * 이때 1+8을 고려하지 않는 이유는 성냥개비 1개로 만들 수 있는 수는 없기 때문이다.
+         * 이처럼 만들 수 없는 수인 1,0은 고려하지 않아야 한다.
+         * 따라서 위에 적은 경우의 수를 모두 비교하여 가장 작은 수를 찾으면 된다.
+         * 이때 주의해야할 점은 비교대상은 성냥개비로 만들어진 숫자이다.
+         *
+         * 점화식은 mindp[n]=(mindp[n-2]+addmin[2], mindp[n-3]+addmin[3], ..., mindp[n-7]+addmin[7])
+         */
+        for (j in 9..100) {
+            for (k in 2..7) {
+                val line = "" + minDp[j - k] + add[k - 2]
+                minDp[j] = Math.min(line.toLong(), minDp[j])
             }
         }
 
-        // 세로 일렬 탐색 for문 i, j 바꾸기!!
-        for(j in 0 until N){
-            count = 1
-            for (i in 0 until N-1){
-                if (adj[i][j] == adj[i+1][j]) {
-                    count++
-                    answer = max(answer, count)
-                }
-                else {
-                    count = 1
-                }
-            }
+        /*
+        * 큰 수는 1과 7만을 사용해서 만들 수 있다.
+        * 성냥개비 8개를 사용해서 만들 수 있는 가장 큰 수는 1111
+        * 성냥개비 9개를 사용해서 만들 수 있는 가장 큰 수는 711
+        * 성냥개비 10개를 사용해서 만들 수 있는 가장 큰 수는 1111
+        * 성냥개비 11개를 사용해서 만들 수 있는 가장 큰 수는 71111
+        *
+        * 즉, 큰 숫자를 만든다고 큰 수가 되는 것이 아닌 만드는데 성냥개비가 가장 적게드는 1을 사용하여
+        * 가장 긴 자릿수를 만드는 것이 큰 수를 만드는 방법이다.
+        * 이때 성냥개비의 갯수가 짝수일 경우는 만들어지는 1의 개수가 딱 떨어지지만
+        * 홀수인 경우에는 성냥개비 1개로 만들 수 있는 수가 없기 때문에 3개로 만들 수 있는 7을 사용해야 한다.
+        *
+        * 따라서 큰 수는 1과 7만을 사용해서 만들 수 있다.
+        */
+        val max = StringBuilder()
+        val a = (matchesCount / 2).toLong()
+        val b = (matchesCount % 2).toLong()
+        if (b == 1L) {
+            max.append("7")
+        } else {
+            max.append("1")
         }
-
-    }
-
-
-    for (i in 0 until N){
-        for (j in 0 until N){
-            // 양옆 스왑
-            if (j+1 < N){
-                val a = adj[i][j]
-                val b = adj[i][j+1]
-                // 서로 스왑하고
-                adj[i][j] = b
-                adj[i][j+1] = a
-                // 가장 긴 연결 요소 구하기
-                search()
-                // 스왑한거 되돌려놓기
-                adj[i][j] = a
-                adj[i][j+1] = b
-            }
-
-            // 위아래 스왑
-            if (i+1 < N){
-                val a = adj[i][j]
-                val b = adj[i+1][j]
-                // 서로 스왑하고
-                adj[i][j] = b
-                adj[i+1][j] = a
-                // 가장 긴 연결 요소 구하기
-                search()
-                // 스왑한거 되돌려놓기
-                adj[i][j] = a
-                adj[i+1][j] = b
-            }
+        for (j in 1 until a) {
+            max.append("1")
         }
-
+        println(minDp[matchesCount].toString() + " " + max.toString())
     }
-
-    println(answer)
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
